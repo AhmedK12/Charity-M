dc5b2dad4eb0d9c2b6faf83ccec3da7e3efbe300
Kamaran Ahmed <kamaran.ahmed@deeptek.ai>
2023-07-12 00:20:11 +0530

    Project SetUp

---
+++ src/main/java/com/example/helptek/HelptekApplication.java
@@ -0,0 +1,13 @@
+package com.example.helptek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelptekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelptekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/helptek/HelptekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helptek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelptekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
